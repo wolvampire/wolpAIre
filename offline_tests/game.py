fba61f1900343281c40adb105590b0a3e1d2d62e
Benoit Matet <benoit@MacBook-Pro-de-Benoit.local>
2019-02-25 14:54:45 +0100

    small chnge on game.py

--- offline_tests/game.py
+++ offline_tests/game.py
@@ -10,21 +10,29 @@ from board_tile import board_tile
 
 
 class game():
-    def __init__(self, id_game):
+    def __init__(self, id_game, n, m):
         self.__board = [[]]
-        self.__n = 0
-        self.__m = 0
+        self.__n = n
+        self.__m = m
         self.id_game = id_game
         self.orders
         self.init_random_state = random.getstate()  # to get the same results later when we reload the game and re-simulate the orders
-
-
-    def reload()
-        random.setstate(self.init_random_state)
-        print random.random()
-
-        # You can also restore the state into your own instance of the PRNG, to avoid
-        # thread-safety issues from using the default, global instance.
-        prng = random.Random()
-        prng.setstate(old_state)
-        print prng.random()
+        self.prng = random.Random()
+        print(prng.random())
+    
+    
+    def create_board(self):
+        self.__board = [[board_tile(x,y,randint(1,10),"HUM") if random()<P_hum else board_tile(x,y) for y in range(self.__m)] for x in range(self.__n)]
+       
+        self.__board[1][1]=board_tile(1,1,10,"WERE")
+        self.__board[self.__n-2][self.__m-2]=board_tile(self.__n-2,self.__m-2,10,"VAMP")
+        
+        
+    def get_board(self):
+        return self.__board
+        
+        
+    def reload(self):
+        self.prng.setstate(self.init_random_state)
+        print(prng.random())
+        
