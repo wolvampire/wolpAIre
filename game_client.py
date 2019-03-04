e70089c789b527a83f505784951afabc6a0e0bf6
Etienne <etienne@DESKTOP-IMIIADJ.localdomain>
2019-03-04 16:43:39 +0100

    qsd

--- game_client.py
+++ game_client.py
@@ -18,7 +18,7 @@ class GameClient():
     def start_connection(self):
         self.__connection = ServerCon(self)
         self.__connection.connect_to_server("127.0.0.1", 6666)
-        self.__connection.send_nme("test")
+        self.__connection.send_nme(self.__decider.get_name())
         while self.__connection.is_connected():
             self.__connection.listen()
 
@@ -52,11 +52,16 @@ class GameClient():
         return True
 
     def callback_upd(self, changesInfosList):
-        update_success =  self.update_map(changesInfosList)
+        update_success = self.update_map(changesInfosList)
         if update_success:
+            print("Waiting ...")
             time.sleep(1)
+            print("Computing ...")
             moves = self._decide()
+            print("Computing done.")
             self.__connection.send_mov(moves)
+        else:
+            print("Failed update, did not decide.")
         return update_success
 
     def callback_map(self, tilesInfosList):
