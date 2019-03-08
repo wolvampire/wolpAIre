72273d7c68c57ee60e5ec552ef5cf0bfe696e389
Etienne <etienne@DESKTOP-IMIIADJ.localdomain>
2019-03-08 10:50:00 +0100

    Start of rule check

--- decider.py
+++ decider.py
@@ -1,4 +1,5 @@
-
+from board import *
+from board_tile import *
 
 class Decider():
     def __init__(self):
@@ -9,8 +10,43 @@ class Decider():
         return self._name
 
     def decide(self, board):
+        '''
+        Returns a list of moves, in the shape (x_origin, y_origin, number, x_tar, y_tar)
+        '''
+        self._board = board
+        ret = self._decide(self._board)
+        if not check_move(ret):
+            print("Warning : he move is not valid !")
+        return ret
+
+    def _decide(self, board):
         print("THIS SHOULD NOT BE CALLLED WHAT HAVE YOU DONE")
         '''
         Returns a list of moves, in the shape (x_origin, y_origin, number, x_tar, y_tar)
         '''
         return [[]]
+
+    def check_move(self, moves):
+        rslt = True
+        #Rule 1
+        if len(moves) == 0:
+            print("Rule 1 broken : There is no move.")
+            rlst = False
+        #Check message integrity
+        for move in moves:
+            if len(move) != 5:
+                print("Integrity broken : A move has  length of {} instead of 5 (move : {}).".format(len(move), move))
+                rslt = False
+                return rslt
+            if move[0] not in range(0, self._board.width) or\
+               move[3] not in range(0, self._board.width) or\
+               move[2] not in range(0, self._board.height) or\
+               move[4] not in range(0, self._board.height):
+                print("Integrity broken : tile out of bounds (board [{}|{}], move : {}).".format(self._board.width, self._board.height, move))
+                rslt = False
+                return rslt
+        #Rule 2
+        for move in moves:
+            if self._board(move[0], move[1]).relation != Relation.ally
+        
+        return rslt
